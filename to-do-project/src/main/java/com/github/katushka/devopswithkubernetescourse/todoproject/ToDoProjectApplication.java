83e41cd48dd54b307e1f08bee6ce571e5dda3429
Yekaterina Polovinkina <kat.ushka403@gmail.com>
2022-07-28 09:57:30 +0300

    Application for Exercise 1.02

---
+++ to-do-project/src/main/java/com/github/katushka/devopswithkubernetescourse/todoproject/ToDoProjectApplication.java
@@ -0,0 +1,23 @@
+package com.github.katushka.devopswithkubernetescourse.todoproject;
+
+import com.github.katushka.devopswithkubernetescourse.todoproject.service.ServerPortService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.message.FormattedMessage;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class ToDoProjectApplication {
+
+    private static final Logger logger = LogManager.getLogger(ToDoProjectApplication.class);
+
+    public static void main(String[] args) {
+        ApplicationContext context = SpringApplication.run(ToDoProjectApplication.class, args);
+        ServerPortService portService = context.getBean(ServerPortService.class);
+        logger.atDebug().log(new FormattedMessage("Server started in port %s", portService.getPort()));
+    }
+
+}
